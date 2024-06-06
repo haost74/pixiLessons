4192cad9ea73a452931f10bd3ad89d0ebbf73c22
Aleksey <haost@yandex>
2024-06-06 16:38:50 +0300

    lessons 12

--- src/lessons/lessons12.js
+++ src/lessons/lessons12.js
@@ -8,33 +8,95 @@ import { EventSystem } from "@pixi/events";
 
 import { NineSlicePlane } from '@pixi/mesh-extras'
 import { Text, TextStyle } from '@pixi/text'
+import { Loader } from '@pixi/loaders'
+import { TilingSprite } from '@pixi/sprite-tiling'
 
-import rect from '../rect.png'
+import back from '../parallax-forest-back-trees.png'
+import front from '../parallax-forest-front-trees.png'
+import lights from '../parallax-forest-lights.png'
+import middle from '../parallax-forest-middle-trees.png'
+import { spread } from 'axios'
 
 // https://pixijs.com/8.x/guides/components/interaction
 
 
-export class Start{
+export class Start {
     app
-    constructor(){
-        this.app = new Application({resizeTo: window, backgroundColor:"0xAAAAAA"})
+    loader
+    bgBack
+    bgMiddle
+    bgFront
+    bgX = 0
+    bgSpeed = 1
+    constructor() {
+        this.app = new Application({ resizeTo: window, backgroundColor: "0xAAAAAA" })
         document.getElementById('root').appendChild(this.app.view)
         const events = new EventSystem(this.app.renderer);
         events.domElement = this.app.renderer.view;
-       
-        let sprite = Sprite.from(rect)
-        sprite.anchor.set(0.5);
 
-            sprite.x = this.app.screen.width  / 2
-            sprite.y = this.app.screen.height / 2
-            this.app.stage.addChild(sprite);
-            sprite.on('pointermove', (e) => {
-                let pos = e.global
-                sprite.x = pos.x
-                sprite.y = pos.y
+        this.loader = new Loader()
+        this.loader
+            .add('back', back)
+            .add('front', front)
+            .add('lights', lights)
+            .add('middle', middle)
+
+        this.loader.onComplete.add((e) => {
+
+            this.bgBack = this.cretaBg(this.loader.resources['back'].texture)
+            this.bgMiddle = this.cretaBg(this.loader.resources['middle'].texture)
+            this.bgFront = this.cretaBg(this.loader.resources['front'].texture)
+
+            document.addEventListener('keyup', (e) => {this.swichDir(e)})
+
+
+            this.app.ticker.add(() => {
+                this.loop();
             })
-            sprite.eventMode = 'static';
+        });
 
-     }
-     
+        this.loader.load();
+
+    }
+
+
+    cretaBg(texture) {
+        let tiling = new TilingSprite(texture, 240, 160)
+        tiling.position.set(0, 0)
+        this.app.stage.addChild(tiling)
+        return tiling
+    }
+
+    loop() {
+     this.updateBg()
+
+    }
+
+    updateBg(){
+        this.bgX = this.bgX + this.bgSpeed
+        
+        this.bgFront.tilePosition.x = this.bgX
+        this.bgMiddle.tilePosition.x = this.bgX /2 
+        this.bgBack.tilePosition.x = this.bgX / 4
+    }
+
+    swichDir(e){
+         console.log(e.keyCode)
+        switch (e.keyCode) {
+            case 37:  //left
+            console.log(this.bgSpeed)
+                this.bgSpeed = this.bgSpeed -1
+                break;
+                case 39:  //
+                this.bgSpeed = this.bgSpeed + 1
+                break;
+                case 32:  //
+                this.bgSpeed =0
+                break;
+        
+            default:
+                break;
+        }
+    }
+
 }
